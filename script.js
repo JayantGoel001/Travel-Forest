c06d66303b4fc718bf510f94eeb4f3af49b4a4ee
CormacKrum <54479676+CormacKrum@users.noreply.github.com>
2020-07-16 12:46:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Travel Forest");
+function enterSite() {
+    $("#landing").css("transform","translateX(-200vw)");
+    $("#dimmed-bg").css("visibility","visible");
+    $("#dimmed-bg").css("transform","translateX(0)");
+
+}
