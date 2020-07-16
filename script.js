889a4273f592dcf082e79369f4386ebc435f698e
CormacKrum <54479676+CormacKrum@users.noreply.github.com>
2020-07-16 13:18:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ function enterSite() {
     $("#landing").css("transform","translateX(-200vw)");
     $("#dimmed-bg").css("visibility","visible");
     $("#dimmed-bg").css("transform","translateX(0)");
-
+    $("#full-site").css("visibility","visible");
+    $("#full-site").css("overflow","auto");
+    $("#full-site").css("max-height","auto");
+    $("#above-fold").css("opacity","1");
 }
