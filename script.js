aa51f241addc378af0d92709085f9d7c067e8bb7
JayantGoel001 <jgoel92@gmail.com>
2021-06-13 18:34:30 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function enterSite(){
+    $("#landing").css("transform","translate(-200vw)");
+    $("#dimmed-bg").css("visibility","visible");
+    $("#dimmed-bg").css("transform","translate(0)");
+}
