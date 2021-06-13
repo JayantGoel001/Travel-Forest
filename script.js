753e988ed341cc8f49646486a434cb5ea983af1e
JayantGoel001 <jgoel92@gmail.com>
2021-06-13 19:07:51 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ function enterSite(){
     $("#landing").css("transform","translate(-200vw)");
     $("#dimmed-bg").css("visibility","visible");
     $("#dimmed-bg").css("transform","translate(0)");
+
+    $("#above-fold").css("opacity","1");
+
+    $("#full-site").css("visibility","visible");
+    $("#full-site").css("overflow","auto");
+    $("#full-site").css("max-height","auto");
+
 }
